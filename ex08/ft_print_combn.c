452db9014e277018f65cfbb70068a6e9396f55d1
FranciscoLi2000 <francisco2000li@icloud.com>
2025-03-27 15:37:45 +0000

    modificar

--- ex08/ft_print_combn.c
+++ ex08/ft_print_combn.c
@@ -3,26 +3,48 @@
 void    ft_print_combn(int n)
 {
     char    arr[9];
-    int         i;
-    int         j;
+    int     i;
+    int     j;
 
+    // Inicializar el array con los valores '0' a 'n-1'
     i = -1;
-    while (++i < n && i >= 0)
-    {
+    while (++i < n)
         arr[i] = i;
-        write(1, &arr[i], 1);
-    }
-    j = n - 1;
-    while (j >= 0 && !(arr[j] == 10 - n + j && (j--)))
+
+    // Primera impresión
+    i = -1;
+    while (++i < n)
     {
-        if (j < 0)
-            return ;
-        arr[j] += 1 + 0 * (i = j);
+        char c = arr[i] + '0';
+        write(1, &c, 1);
+    }
+
+    // Generar combinaciones
+    while (1)
+    {
+        j = n - 1;
+        while (j >= 0 && arr[j] == 10 - n + j)
+            j--;
+
+        if (j < 0)  // Salimos si no hay más combinaciones
+            return;
+
+        arr[j]++;
+
+        // Reajustar valores siguientes
+        i = j;
         while (++i < n)
             arr[i] = arr[i - 1] + 1;
+
+        // Imprimir separador
         write(1, ", ", 2);
+
+        // Imprimir la nueva combinación
         i = -1;
         while (++i < n)
-            write(1, &arr[i], 1);
+        {
+            char c = arr[i] + '0';
+            write(1, &c, 1);
+        }
     }
 }

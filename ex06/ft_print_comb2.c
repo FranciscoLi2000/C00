c5bb268764e274fa2e62f05836d7f0dc75d9682b
FranciscoLi2000 <francisco2000li@icloud.com>
2025-03-27 15:32:34 +0000

    modificar

--- ex06/ft_print_comb2.c
+++ ex06/ft_print_comb2.c
@@ -1,5 +1,10 @@
 #include <unistd.h>
 
+void ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
 void ft_print_comb2(void)
 {
     int x;
@@ -8,7 +13,7 @@ void ft_print_comb2(void)
     x = 0;
     while (x <= 98)
     {
-        y = y + 1;
+        y = x + 1;  // CORRECCIÓN: reiniciar `y` en cada iteración de `x`
         while (y <= 99)
         {
             ft_putchar(x / 10 + '0');
@@ -17,7 +22,7 @@ void ft_print_comb2(void)
             ft_putchar(y / 10 + '0');
             ft_putchar(y % 10 + '0');
             if (!(x == 98 && y == 99))
-                    write(1, ", ", 2);
+                write(1, ", ", 2);
             y++;
         }
         x++;
